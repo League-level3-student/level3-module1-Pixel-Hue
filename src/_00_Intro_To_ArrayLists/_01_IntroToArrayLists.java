b32abbb1fd70c99854b8a336a4b02f8619f06d9e
GavinB <GavinB@hi.attlocal.net>
2023-02-26 16:29:38 -0800

    fdsafad

--- src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
+++ src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
@@ -1,21 +1,49 @@
 package _00_Intro_To_ArrayLists;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class _01_IntroToArrayLists {
     public static void main(String[] args) {
+    	char[] eCheck;
         // 1. Create an array list of Strings
         //    Don't forget to import the ArrayList class
-
+ArrayList<String> list = new ArrayList<>();
         // 2. Add five Strings to your list
-
+list.add("John");
+list.add("Joe");
+list.add("Lan");
+list.add("Tanner");
+list.add("Pic");
         // 3. Print all the Strings using a standard for-loop
-
-        // 4. Print all the Strings using a for-each loop
-
-        // 5. Print only the even numbered elements in the list.
-
-        // 6. Print all the Strings in reverse order.
-
-        // 7. Print only the Strings that have the letter 'e' in them.
-        
-    }
+for (int i = 0; i < list.size(); i++) {
+	System.out.println(list.get(i)); 
 }
+        // 4. Print all the Strings using a for-each loop
+for (String string : list) {
+	System.out.println(string);
+}
+        // 5. Print only the even numbered elements in the list.
+System.out.println("Even numbers");
+for (int i = 0; i < list.size(); i+=2) {
+	System.out.println(list.get(i));
+}
+        // 6. Print all the Strings in reverse order.
+System.out.println("Reverse numbers");
+for (int i = list.size() -1; -1 < i; i-=1) {
+	System.out.println(list.get(i));
+}
+        // 7. Print only the Strings that have the letter 'e' in them.
+System.out.println("Names with E");
+for (int i = 0; i < list.size(); i++) {
+	eCheck = list.get(i).toCharArray();
+	
+	for (int j = 0; j < eCheck.length; j++) {
+		if (eCheck[j] == 'e') {
+			System.out.println(list.get(i)); 
+		}
+	}
+}
+    }
+    }
+
--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,6 +1,49 @@
 package _00_Intro_To_ArrayLists;
 
-public class _02_GuestBook {
+import java.awt.Window;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class _02_GuestBook implements ActionListener {
+	static JPanel panel; 
+	static JFrame frame;
+	static JButton button1;
+	static JButton button2;
+	static JLabel label1 = new JLabel();
+	static JLabel label2 = new JLabel();
+	
+	public static void main(String[] args) {
+		//setup frame
+	panel = new JPanel();
+	frame = new JFrame();
+	button1 = new JButton();
+	button2 = new JButton();
+	frame.add(panel);
+	panel.add(button1);
+	panel.add(button2);
+	
+	label1.setText("Add Name");
+	label2.setText("View Names");
+	
+	button1.add(label1);
+	button2.add(label2);
+	
+	//pack and make frame visible
+	frame.pack();
+	frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//change this
+		//JButton buttonClicked = e.getSource();
+	}
+	
     /*
      * Create a GUI with two buttons. One button reads "Add Name" and the other
      * button reads "View Names". When the add name button is clicked, display
