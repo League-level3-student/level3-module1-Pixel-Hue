525cb726173afabaf52a436b9dc3e53902a6ccc4
GavinB <GavinB@hi.attlocal.net>
2023-03-05 16:29:23 -0800

    gsdfgsd

--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -5,10 +5,12 @@ import java.awt.Window;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 public class _02_GuestBook implements ActionListener {
 	static JPanel panel; 
 	static JFrame frame;
@@ -16,8 +18,12 @@ public class _02_GuestBook implements ActionListener {
 	static JButton button2;
 	static JLabel label1 = new JLabel();
 	static JLabel label2 = new JLabel();
-	
+	ArrayList<String> guestBook = new ArrayList<>();
+	Object[] guestArray;
+	String guestBookView;
 	public static void main(String[] args) {
+		_02_GuestBook gb = new _02_GuestBook();
+		
 		//setup frame
 	panel = new JPanel();
 	frame = new JFrame();
@@ -29,19 +35,37 @@ public class _02_GuestBook implements ActionListener {
 	
 	label1.setText("Add Name");
 	label2.setText("View Names");
-	
+
 	button1.add(label1);
 	button2.add(label2);
-	
+
 	//pack and make frame visible
+	gb.setup();
 	frame.pack();
 	frame.setVisible(true);
+
+	}
+	public void setup() {
+		button1.addActionListener(this);
+		button2.addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		//change this
-		//JButton buttonClicked = e.getSource();
+		Object buttonClicked = e.getSource();
+		if (buttonClicked == button1) {
+			String nameAdd = JOptionPane.showInputDialog("Type the name you would like to add...");
+			guestBook.add(nameAdd);
+			JOptionPane.showMessageDialog(button1, "Name Added!");
+		} else if (buttonClicked == button2) {
+			guestArray = guestBook.toArray();
+			for (int i = 0; i < guestArray.length; i++) {
+int j = i+1;
+				guestBookView += "Guest #" + j + ": " + guestArray[i].toString() + "\n";
+			}
+			JOptionPane.showMessageDialog(button2, guestBookView);
+		}
 	}
 	
     /*
