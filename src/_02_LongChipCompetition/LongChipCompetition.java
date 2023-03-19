28dc056cf2b0aeabf9f037169701f679afb0a662
GavinB <GavinB@hi.attlocal.net>
2023-03-19 16:28:43 -0700

    fdsafdsa

--- src/_02_LongChipCompetition/LongChipCompetition.java
+++ src/_02_LongChipCompetition/LongChipCompetition.java
@@ -1,6 +1,7 @@
 package _02_LongChipCompetition;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class LongChipCompetition {
@@ -15,9 +16,32 @@ public class LongChipCompetition {
 
     public static void main(String[] args) {
         LongChipCompetition lcc = new LongChipCompetition();
-       // lcc.
+        lcc.initializeBeatles();
+      lcc.checkLongestChip();
     }
-
+    public void checkLongestChip() {
+    	//Get the longest chip of each beatle has and put all the nums in a list of integers
+    	ArrayList<Double> chipList = new ArrayList<Double>();
+    	
+    	for (int j = 0; j < theBeatles.size(); j++) {	
+    		ArrayList<Double> tempList = new ArrayList<Double>();
+    		for (int i = 0; i < theBeatles.get(j).getChips().size(); i++) {
+    		 tempList.add(theBeatles.get(j).getChips().get(i).getLength());
+			}
+    		chipList.add(Collections.max(tempList)); //biggest length french fry this beatle has
+  
+		System.out.println(Collections.max(tempList));
+    	}
+		//check which num is biggest from the list
+    	for (int i = 0; i < chipList.size(); i++) {
+			if (chipList.get(i) == Collections.max(chipList)) {
+				System.out.println(theBeatles.get(i).getName() + " has the longest chip!");
+			}
+		}
+		
+			
+		
+    }
     private void initializeBeatles() {
         Beatle george = new Beatle("George");
         Beatle john = new Beatle("John");
